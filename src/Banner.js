eff164ab09f6488a60de35ad44c37aa8106f5069
Aditya Mali <41832893+maliaditya@users.noreply.github.com>
2021-04-19 18:49:38 +0530

    project structuring

---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './banner'
+const Banner = () => {
+  return (
+    <div>
+      <h1>Banner</h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './projects.css'
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
