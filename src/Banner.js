364ebe7fd9e982267e6659491ad06457bf2b102a
aditya <adityamali33@gmail.com>
2021-04-30 00:27:43 +0530

    get in touch added

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './banner.css'
+import { FaTelegramPlane } from 'react-icons/fa'
 const Banner = () => {
   return (
     <>
@@ -8,7 +9,9 @@ const Banner = () => {
           <h1>I’M A FULLSTACK DEVELOPER</h1>
           <h1>I MAKE WEBSITES/WEBAPPS</h1>
           <button className='btn '>
-            <span>CONTACT ME</span>
+            <span>
+              <FaTelegramPlane /> GET IN TOUCH
+            </span>
           </button>
         </pre>
       </section>
