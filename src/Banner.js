e1dfb44c7f54a113e7cc136cd6acb93ff75673c1
aditya <adityamali33@gmail.com>
2021-04-30 06:08:52 +0530

    projects added

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './banner.css'
+import Modal from './Modal'
+import { useGlobalContext } from './context'
 import { FaTelegramPlane } from 'react-icons/fa'
+
 const Banner = () => {
+  const { openModal } = useGlobalContext()
   return (
     <>
+      <Modal />
+
       <section className='banner'>
         <pre className='wrapper'>
           <h1>I’M A FULLSTACK DEVELOPER</h1>
           <h2>I MAKE WEBSITES/WEBAPPS</h2>
-          <button className='btn '>
+          <button className='btn ' onClick={openModal}>
             <span>
               <FaTelegramPlane /> GET IN TOUCH
             </span>
--- src/Projects.js
+++ src/Projects.js
@@ -3,10 +3,47 @@ import './projects.css'
 const Projects = () => {
   return (
     <div className='container'>
-      <h1>Projects</h1>
-      <hr />
+      <div className='row'>
+        <div className='column'>
+          <div className='card' style={{ width: '18rem' }}>
+            <img
+              src='https://res.cloudinary.com/dswrobbrp/image/upload/v1618923768/patrick-pahlke-G7wgKn7j_Rs-unsplash_gvruns.jpg'
+              className='card-img-top'
+              alt='Photography Website'
+            />
+            <div className='card-body'>
+              <p className='card-text'>Photography Website</p>
+              <a
+                className='btn'
+                href='http://aditya1998.pythonanywhere.com/'
+                style={{ color: 'black', fontSize: '0.8rem' }}
+              >
+                preview
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='column'>
+          <div className='card' style={{ width: '18rem' }}>
+            <img
+              src='https://res.cloudinary.com/dswrobbrp/image/upload/v1619742060/taras-shypka-86b0GW7aLUw-unsplash_ou4wpm.jpg'
+              className='card-img-top'
+              alt='Online editor'
+            />
+            <div className='card-body'>
+              <p className='card-text'>Online Editor</p>
+              <a
+                className='btn'
+                href='https://radiant-ridge-17248.herokuapp.com/'
+                style={{ color: 'black', fontSize: '0.8rem' }}
+              >
+                preview
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Projects
